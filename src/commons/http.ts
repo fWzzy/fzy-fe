532028b46b64a2896f7601a2030c1fa5276c94d4
fWzzy <chung.pt912@gmail.com>
2023-02-07 14:48:33 +0700

    fix axios types

--- src/commons/http.ts
+++ src/commons/http.ts
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios, { AxiosHeaders } from 'axios'
 import UserSession from './user-session'
 
 const http = axios.create()
@@ -7,11 +7,13 @@ http.defaults.headers.post['Content-Type'] = 'application/json'
 http.defaults.headers.put['Content-Type'] = 'application/json'
 
 http.interceptors.request.use((config) => {
-  config.headers = config.headers ?? {};
+  // config.headers = config.headers ?? {};
   const authToken = UserSession.getToken()
   if (authToken) {
     const token = `Bearer ${authToken}`
-    config.headers['Authorization'] = token
+    // config.headers['Authorization'] = token
+    config.headers = { ...config.headers } as AxiosHeaders;
+    config.headers.set('Authorization', token);
   }
 
   return config
